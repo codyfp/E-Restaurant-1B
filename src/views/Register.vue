179f60673eee8310c7d6fab67c4afd7a189208c7
Uddhav Parashar <uddhavparashar6@gmail.com>
2020-09-06 18:01:24 +1000

    Updating... Need to redo the script for the signin authentication functions

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,54 @@
 <template>
-  <p> GU</p>
+  <div class= "signup container">
+    <h1>Register</h1>
+    <form @submit.prevent="signup" class="card-panel">
+      <div class="field">
+        <label for="fullName">Full Name:</label>
+        <input type="name" name="fullName" v-model="fullName">
+      </div>
+      <div class="field">
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model="email">
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password">
+      </div>
+      <div class="field center">
+        <button class="btn yellow">Register</button>
+      </div>
+    </form>  
+  </div>
 </template>
 
 <script>
+import slugify from 'slugify'
   export default {
-    name: 'HelloWorld',
-
-    data: () => ({
-      
-    }),
-  }
+    name: 'Signup',
+    data(){
+      return{
+        email: null,
+        password: null,
+        fullName: null,
+      }
+    },
+    methods:{
+      signup(){
+        
+      }
+    }
+  } 
 </script>
+
+<style>
+.signup{
+  max-width: 400px;
+  margin-top: 60px;
+}
+.signup h2{
+  font-size:2.4em;
+}
+.signup .field{
+  margin-bottom: 16px;
+}
+</style>
