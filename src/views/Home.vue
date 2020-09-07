9fce21ce760a254074827e77db07b24891968ee1
Anesu <anesu4@yahoo.com.au>
2020-09-07 17:36:49 +1000

    Update: Bug Fixing - 7/9/20

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,20 @@
       <v-app-bar color="deep-purple accent-4" dense dark>
         <v-app-bar-nav-icon @click.stop="nav = !nav"></v-app-bar-nav-icon>
 
-        <v-toolbar-title>e-Food</v-toolbar-title>
-        <!-- //  -->
+        <v-toolbar-title>Le Bistrot d'Andre</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <a href="/booking">
+            Booking
+          </a>
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon> <a href="/menu"></a>Menu </v-btn>
+
         <v-spacer></v-spacer>
         <!-- // Sign Up / Login -->
         <v-btn icon>
@@ -15,17 +27,13 @@
         </v-btn>
 
         <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-
-        <v-btn icon>
           <v-icon>mdi-dots-vertical</v-icon>
         </v-btn>
       </v-app-bar>
 
-      <v-navigation-drawer v-model="nav">
+      <!-- <v-navigation-drawer v-model="nav">
         <v-list></v-list>
-      </v-navigation-drawer>
+      </v-navigation-drawer>-->
 
       <v-container>
         <v-layout></v-layout>
