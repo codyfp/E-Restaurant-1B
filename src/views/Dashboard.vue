0797e13070679bba3ad74e55ecc72f1b8cc85b0c
Anesu <anesu4@yahoo.com.au>
2020-10-06 15:24:47 +1100

    Development - Anesu: Push all of updated information for Dashboard into a new branch

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -43,6 +43,15 @@ html {
   padding: 20px;
   text-align: center;
 }
+.main-text {
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  border: 3px solid #f1f1f1;
+  padding: 20px;
+  text-align: center;
+}
 .bg-text-style {
   background-color: rgb(0, 0, 0); /* Fallback color */
   background-color: rgba(0, 0, 0, 0.4); /* Black w/opacity/see-through */
@@ -64,82 +73,298 @@ html {
   filter: blur(8px);
   -webkit-filter: blur(8px);
 }
-</style>
- 
-<template>
-  <div class="bg-image"><!-- 
-    <v-app-bar color="deep-purple accent-4" dense dark> -->
-    <v-container fluid>
-      <v-layout>
-        <!-- <v-col>
-          <v-row>
-            <v-col>
-              <v-img height="100" width="100" src="../assets/baguette.jpg"></v-img>
-            </v-col>
-            <v-col></v-col>
-          </v-row>
-        </v-col>-->
-        <div id="maps"></div>
-        <div class="blur">
-          <h1>HellWorld</h1>
-        </div>
-        <!-- <div align="center">
-          <h1 style="text-align: center;">Le Bistrot d'Andre</h1>
-        </div>-->
+.bg {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: url("./img/bg.jpg");
+  background-size: cover;
+}
+.schedule {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  text-align: center;
+}
 
-        <div class="bg-text">
-          <h1>BOOKINGS NOW AVAILABLE</h1>
-          <h2>69 Commonwealth St, Surry Hills NSW 2010</h2>
-          <h3>02 9281 3322</h3>
-          <h3>eat@lebistrot.com.au</h3>
-        </div>
-        <div>
-          <v-row class="bg-text-style">
-            <div class="text-center">
-              <v-btn color="indigo" class="ma-2" tile dark href="/booking">Book Now</v-btn>
-            </div>
-            <v-spacer></v-spacer>
-            <div class="text-center">
-              <!-- <v-btn rounded color="primary" dark href="/menu">Menu</v-btn> -->
-              <v-btn class="ma-2" tile color="indigo" dark href="/menu">Menu</v-btn>
-            </div>
-          </v-row>
-        </div>
-        <!-- <v-img
-          src="https://e7.pngegg.com/pngimages/686/914/png-clipart-sad-frog-illustration-pepe-the-frog-sadness-alt-right-meme-sad-leaf-animals.png"
-        ></v-img>-->
+.schedule td,
+.schedule th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+.schedule tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+.schedule tr:hover {
+  background-color: #ddd;
+}
+
+.schedule th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #009688;
+  color: white;
+  text-align: center;
+}
+
+.schedule v-btn {
+  cursor: pointer;
+}
+</style>
+
+<template>
+  <div class="bg-image">
+    <v-container>
+      <v-layout row>
+        <div id="maps"></div>
+        <v-flex>
+          <div class="main-text">
+            <!-- <h1>Welcome, {{ user.email }}!</h1> -->
+            <!-- <v-btn color="white" text rounded class="my-2 right-btn">Logged in as: {{ user.name }}</v-btn> -->
+          </div>
+
+          <v-col style="margin-top: 100px;">
+            <v-card class="main-text" min-height="720" flat>
+              <v-card-text>
+                <h1>Current Bookings</h1>
+                <v-spacer class="pb-5"></v-spacer>
+              </v-card-text>
+              <v-card class="d-flex justify-center" flat>
+                <table class="schedule">
+                  <tr>
+                    <th>Number</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Seats</th>
+                    <th>Location</th>
+                    <th>Menu</th>
+                    <th>Booking</th>
+                  </tr>
+                  <tr v-for="booking in filteredBookings" :key="booking.id">
+                    <td>{{ placement(booking.id) + 1 }}</td>
+                    <td>
+                      {{ booking.datetime.seconds | moment("DD MMM YYYY") }}
+                    </td>
+                    <td>{{ booking.datetime.seconds | moment("h:mm a") }}</td>
+                    <td>{{ booking.seat }}</td>
+                    <td>Surry Hills</td>
+                    <td>
+                      <v-btn
+                        color="primary"
+                        :disabled="orderDisable(booking.datetime.seconds)"
+                        >Edit Menu
+                      </v-btn>
+                    </td>
+                    <td>
+                      <v-btn
+                        color="error"
+                        @click="editBook(booking.id)"
+                        :disabled="orderDisable(booking.datetime.seconds)"
+                        >Edit Booking
+                      </v-btn>
+                    </td>
+                  </tr>
+                </table>
+              </v-card>
+              <v-dialog v-model="dialog" width="400">
+                <v-card>
+                  <router-link
+                    :to="{
+                      name: 'EditBooking',
+                      params: { id: this.ID },
+                    }"
+                    tag="button"
+                  >
+                    <v-btn
+                      @click="dialog = false"
+                      width="250"
+                      height="50"
+                      class="ma-5"
+                    >
+                      Edit Booking
+                    </v-btn>
+                  </router-link>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    @click="deleteOrder"
+                    width="250"
+                    height="50"
+                    class="ma-2 mb-4"
+                  >
+                    Cancel Booking
+                  </v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn color="error" @click="dialog = false" class="my-5"
+                    >Close</v-btn
+                  >
+                </v-card>
+              </v-dialog>
+            </v-card>
+          </v-col>
+        </v-flex>
       </v-layout>
     </v-container>
   </div>
-
 </template>
 
+<script>
+// import db from "@/firebase/init";
+import firebase from "firebase";
+import db from "@/firebase/init";
 
-<script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALKwFMBtlWJ8nZgTeKTmkFwbH_J0B2pQM&libraries=places&callback=initMap">
+// import EditBooking from "@/views/EditBooking";
+
+export default {
+  data() {
+    return {
+      user: "",
+      date: "",
+      menu: false,
+      bookings: [],
+      dialog: false,
+      ID: 0,
+    };
+  },
+
+  created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user.email;
+      } else {
+        this.user = null;
+      }
+    });
+    let refBookings = db.collection("bookings").orderBy("datetime", "asc");
+    refBookings.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        let doc = change.doc;
+        if (change.type === "removed") {
+          this.edit = true;
+          this.bookings.splice(this.placement(doc.id), 1);
+          this.edit = false;
+        } else if (change.type === "modified") {
+          this.edit = true;
+          this.bookings.splice(this.placement(doc.id), 1, {
+            id: doc.id,
+            UsrID: doc.data().UsrID,
+            datetime: doc.data().datetime,
+            seat: doc.data().seat,
+          });
+          refBookings.orderBy("datetime", "asc");
+          this.edit = false;
+        } else {
+          this.bookings.push({
+            id: doc.id,
+            UsrID: doc.data().UsrID,
+            datetime: doc.data().datetime,
+            seat: doc.data().seat,
+          });
+        }
+      });
+    });
+  },
+
+  computed: {
+    filteredBookings: function() {
+      if (this.user == "staffefooddemo@gmail.com" && this.date.length == 0) {
+        return this.bookings;
+      }
+      return this.bookings.filter((booking) => {
+        if (this.user == "staffefooddemo@gmail.com" && this.date.length > 0) {
+          if (
+            JSON.stringify(
+              this.$moment(booking.datetime.seconds * 1000).format("YYYY-MM-DD")
+            ) == JSON.stringify(this.date)
+          ) {
+            return true;
+          } else {
+            return false;
+          }
+        } else if (this.date.length == 0) return booking.UsrID.match(this.user);
+        else {
+          if (
+            JSON.stringify(
+              this.$moment(booking.datetime.seconds * 1000).format("YYYY-MM-DD")
+            ) == JSON.stringify(this.date)
+          ) {
+            return booking.UsrID.match(this.user);
+          } else {
+            return false;
+          }
+        }
+      });
+    },
+    formattedDate() {
+      return this.formatDate(this.date);
+    },
+  },
+
+  watch: {},
+
+  methods: {
+    editBook(id) {
+      this.ID = id;
+      this.dialog = true;
+    },
+    deleteOrder() {
+      db.collection("bookings")
+        .doc(this.ID)
+        .delete();
+      this.dialog = false;
+    },
+    formatDate(date) {
+      if (!date) return null;
+      return this.$moment(date).format("dddd, Do MMM YYYY");
+    },
+    clearFilter: function() {
+      this.date = "";
+    },
+    placement: function(id) {
+      let array = [];
+      if (this.edit) {
+        array = this.bookings;
+      } else {
+        array = this.filteredBookings;
+      }
+      let place = array
+        .map(function(e) {
+          return e.id;
+        })
+        .indexOf(id);
+      return place;
+    },
+    orderDisable: function(time) {
+      if (this.user == "staffefooddemo@gmail.com") {
+        return false;
+      }
+      time = (time - 7200) * 1000;
+      let now = Date.now();
+      time = time - now;
+      if (time <= 0) {
+        return true;
+      }
+      return false;
+    },
+  },
+};
 </script>
+
+<script
+  async
+  defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALKwFMBtlWJ8nZgTeKTmkFwbH_J0B2pQM&libraries=places&callback=initMap"
+></script>
 
 <script>
 export default {
-	name: "Home",
+  name: "Dashboard",
 };
 
-var locations = [
-  { lat: -33.865143, lng: 151.206 },
-  { lat: -33.8652, lng: 151.21 },
-  { lat: -33.79, lng: 151.2091 },
-  { lat: -33.84, lng: 151.2098 },
-  { lat: -33.9, lng: 149.6 },
-  { lat: -35.9, lng: 149.2 },
-];
-var names = [
-  "Aiden Boal",
-  "Bryan Lifschitz",
-  "Ethel Podany",
-  "Fabian Sanderford",
-  "Gage Wildridge",
-  "Dr Tim",
-];
+var locations = [{ lat: -33.865143, lng: 151.206 }];
+var names = ["Aiden Boal"];
 var markers = [];
 var map;
 var image =
@@ -160,76 +385,6 @@ function initMap() {
         featureType: "administrative.locality",
         elementType: "labels.text.fill",
         stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [{ color: "#263c3f" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#6b9a76" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [{ color: "#38414e" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#212a37" }],
-      },
-      {
-        featureType: "road",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#9ca5b3" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [{ color: "#746855" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#1f2835" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#f3d19c" }],
-      },
-      {
-        featureType: "transit",
-        elementType: "geometry",
-        stylers: [{ color: "#2f3948" }],
-      },
-      {
-        featureType: "transit.station",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [{ color: "#17263c" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#515c6d" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.stroke",
-        stylers: [{ color: "#17263c" }],
       },
     ],
   });
@@ -253,18 +408,18 @@ function createMarker(point, map, index, name) {
 
   var marker = new google.maps.Marker(markerOpts);
 
-  google.maps.event.addListener(marker, "click", function () {
+  google.maps.event.addListener(marker, "click", function() {
     //window.alert(marker.getTitle());
     messageIn(marker.getTitle());
   });
-  google.maps.event.addListener(marker, "mouseover", function () {
+  google.maps.event.addListener(marker, "mouseover", function() {
     //messageIn(marker.getTitle());
   });
 }
 
 function addMarkerWithTimeout(position, timeout, index, name) {
   clearMarkers();
-  window.setTimeout(function () {
+  window.setTimeout(function() {
     markers.push(createMarker(position, map, index, name));
   }, timeout);
   //window.alert(index);
